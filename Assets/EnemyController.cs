2140eeb618374d93bb7e1bf1bc9921c2ab0c71ab
alexjhoel <alexanderalmeida20@gmail.com>
2023-11-09 17:35:32 -0300

    Interacciones entre enemigos, tropas y proyectiles, animaciones basicas establecidas

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour, IDamageable
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject projectilePrefab;
     [SerializeField]
     AnimationCurve movementCurve;
 
@@ -12,33 +13,68 @@ public class EnemyController : MonoBehaviour
     GameObject ghost;
 
     [SerializeField]
+    private float health;
+    [SerializeField]
+    private float attackSpeed;
+    [SerializeField]
+    private ProjectileStats stats;
+    [SerializeField]
     private float movementSpeed;
 
+    [SerializeField]
+    private string attackAnimationName;
 
 
-    float currentHealth = 21;
+
+    float currentHealth;
     float timer;
-    bool attacking = false;
+    float fireTime;
+    
+
     void Start()
     {
-        
+        projectilePrefab.GetComponent<ProjectileController>().projectileStats = stats;
+        currentHealth = health;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (attacking) return;
+        //Este operador << se conoce como bitshift, y es para crear mascaras de colisiones, esto es que definimos cuales layers
+        //queremos detectar como validos para colisionar y disparar proyectil. En este caso decimos que queremos colisionar solo
+        //con el layer 7, que corresponde a tropas alidas.
+        int layerMask = 1 << 7;
+        RaycastHit2D ray;
         timer += Time.deltaTime;
+        if (ray = Physics2D.Raycast(transform.position, Vector2.down, stats.range, layerMask))
+        {
+            //Raycast manda un rayo de largo dependiendo del rango establecido en ProjectileStats, a la primera tropa que colisione
+            //restablece el timer de ataque, y manda el trigger para la animación de ataque
+            if(timer > fireTime)
+            {
+                GetComponent<Animator>().SetTrigger(attackAnimationName);
+                fireTime = timer + attackSpeed;
+            }
+            
+            return;
+
+        }
         transform.position = transform.position + new Vector3(0,-movementCurve.Evaluate(timer % 2 / 2f) * movementSpeed / 1000,0);
+    }
+
+    public void CreateProjectile()
+    {
+        //Event de creación de proyectil mandado por la animación
+        GameObject projectile = Instantiate(projectilePrefab, transform, true);
+        projectile.SetActive(true);
     }
 
     public void TakeDamage(float damage)
     {
         //Recibir daño por parte de proyectiles
         currentHealth -= damage;
-
-        if(currentHealth <= 0)
+        Debug.Log("Enemigo: " + currentHealth);
+        if (currentHealth <= 0)
         {
             //Efecto visual del fantasma
             GameObject newGhost = Instantiate(ghost, null);
@@ -48,30 +84,12 @@ public class EnemyController : MonoBehaviour
 
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Entro");
-        //Al entrar en colisión con tropas aliadas
-        if (collision.gameObject.layer != 7) return;
-        
-        Animator animator = GetComponent<Animator>();
-        //Attacking cambia la animación a atacar
-        animator.SetBool("Attacking", true);
-        attacking = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("Salio");
-        //Al entrar de colisión con tropas aliadas
-        if (collision.gameObject.layer != 7) return;
-        
-        Animator animator = GetComponent<Animator>();
-        
-        animator.SetBool("Attacking", false);
-        attacking = false;
-    }
 }
 
-//
+internal interface IDamageable
+{
+    //Interfaz para recibir daño
+    public void TakeDamage(float damage);
+}
+
+
